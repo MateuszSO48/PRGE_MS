9ab8c7bfc14452996e399503e2d1f927b5e98943
MateuszSO48 <u56148@student.wat.edu.pl>
2025-12-10 09:22:00 +0100

    feat: add mapComponent with ol map

--- config/node/frontend/my-app/src/pages/Services.js
+++ config/node/frontend/my-app/src/pages/Services.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {Button} from "@mui/material";
 
 function Services(props) {
-    return (
+    return (<div>
         <div>Services</div>
+        <Button className="services_button" variant="contained" size="large" component={Link} to="/map">PRZEJDŹ DO MAPY</Button>
+        </div>
     );
 }
 
