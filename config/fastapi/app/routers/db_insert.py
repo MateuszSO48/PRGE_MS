5b39ee60f1859a70e28e9d63288b88bbf0d59208
MateuszSO48 <u56148@student.wat.edu.pl>
2026-01-12 14:44:07 +0100

    feat: add CardComponent

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -8,7 +8,7 @@ from app.settings import db_name, db_user, db_password
 
 def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
-        f"postgresql://{db_user}:{db_password}@postgres:5432/{db_name}"
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
 
 
