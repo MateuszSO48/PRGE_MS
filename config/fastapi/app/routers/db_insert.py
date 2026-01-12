17dc72a25d507c695c1e7133548ee9ec38c4d3eb
MateuszSO48 <u56148@student.wat.edu.pl>
2026-01-12 12:54:22 +0100

    feat: add dynamic requests to backend

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from sqlalchemy import create_engine, text
 router_insert = APIRouter()
 
@@ -11,18 +12,32 @@ def connect_to_db(db_name: str, db_user: str, db_password: str):
     )
 
 
-@router_insert.get("/insert_user")
-async def insert_user():
+class UserData(BaseModel):
+    name:str
+    posts:int
+    location:str
+
+
+@router_insert.post("/insert_user")
+async def insert_user(user: UserData):
     try:
         db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
+        params = {
+            "name": user.name,
+            "posts": user.posts,
+            "location": user.location
+
+        }
+
+
         sql_query = text("""
                          insert into users (name, posts, location)  
-                         VALUES ('Jan', 3, 'Gdańsk'); \
+                         VALUES (:name, :posts, :location); \
                          """)
 
         with db_connection.connect() as conn:
-            result = conn.execute(sql_query)
+            result = conn.execute(sql_query, params)
             conn.commit()
             print(result)
 
---
+++ config/fastapi/app/routers/dynamic_content.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from sqlalchemy import create_engine, text
+from app.settings import db_name, db_user, db_password
+
+router_get_users = APIRouter()
+
+def connect_to_db(db_name: str, db_user: str, db_password: str):
+    return create_engine(
+        f"postgresql://{db_user}:{db_password}@postgres:5432/{db_name}"
+    )
+
+@router_get_users.get("/get_users")
+async def get_users():
+    try:
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
+
+        sqL_query = text(""" select * from users""")
+
+        with db_connection.connect() as conn:
+            result = conn.execute(sqL_query)
+            users={dict(row._mapping) for row in result}
+
+        return {"data": users}
+    except Exception as e:
+        print(f"Błąd podczas get_users")
+        return {"error": str(e)}
