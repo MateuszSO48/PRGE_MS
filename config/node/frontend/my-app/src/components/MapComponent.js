af81ce274eedadc8182522305af7221066c60320
MateuszSO48 <u56148@student.wat.edu.pl>
2026-01-14 12:34:43 +0100

    feat : dodana warstwa budynki

--- config/node/frontend/my-app/src/components/MapComponent.js
+++ config/node/frontend/my-app/src/components/MapComponent.js
@@ -24,14 +24,15 @@ function MapComponent(props) {
                     //http://localhost:9000/geoserver/ne/wms?service=WMS&version=1.1.0&request=GetMap&layers=ne%3Acountries&bbox=-180.0%2C-90.0%2C180.0%2C83.64513&width=768&height=370&srs=EPSG%3A4326&styles=&format=application/openlayers
                     new TileLayer({
                         source: new TileWMS({
-                            url: "http://localhost:9000/geoserver/ne/wms",
+                            url: "http://localhost:9000/geoserver/prge/wms",
                             params: {
-                                'LAYERS': 'ne:countries',
+                                'LAYERS': 'prge:budynki',
                                 'TILES': true
                             },
                             serverType: 'geoserver',
                             transition: 0
-                        })
+                        }),
+
                     })
 
                 ],
