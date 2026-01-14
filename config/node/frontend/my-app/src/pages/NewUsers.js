bb3f1cd9ab43624bc2489eef2e311ed007654091
MateuszSO48 <u56148@student.wat.edu.pl>
2026-01-14 09:43:08 +0100

    wip: form control

--- config/node/frontend/my-app/src/pages/NewUsers.js
+++ config/node/frontend/my-app/src/pages/NewUsers.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Container, Box, TextField} from "@mui/material";
 
 function NewUser(props) {
+    const [userName, setUserName] = useState("")
+    const [userLocation, setUserLocation] = useState("")
+    const [userPosts, setUserPosts] = useState(0)
+
+
     return (
-        <div></div>
+        <div>
+
+            <Container>
+                <Box component="form"
+                >
+                    <TextField
+                        fullWidth
+                        label="imię"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    ></TextField>
+                    <TextField
+                        fullWidth
+                        label="location"
+                        value={userLocation}
+                        onChange={(e) => setUserLocation(e.target.value)}
+                    ></TextField>
+                    <TextField
+                        fullWidth
+                        label="posts"
+                        value={userPosts}
+                        onChange={(e) => setUserLocation(e.target.value)}
+                    ></TextField>
+                </Box>
+            </Container>
+
+
+        </div>
     );
 }
 
